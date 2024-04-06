e61b737d4dfa39f0e10ad3abdcbb38f8459f4519
L33P1n <work.pinlee@gmail.com>
2024-04-06 15:11:33 +0800

    Flask Server is Up

---
+++ server.py
@@ -0,0 +1,13 @@
+## this is a flask server
+
+
+from server import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Test'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
